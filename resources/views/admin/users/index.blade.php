df1511a188cd75393cd0c2df3d38840f22b8f188
Cristiano Santos <santosdesouzacris@gmail.com>
2024-05-29 21:38:37 -0300

    Up files

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Usuários</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($users as $user)
+                <tr>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td> Ações </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="100">Nenhum registro no banco</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+    {{ $users->links() }}
+</body>
+</html>
